99b74a9f81ab2a3f8ec99e83c027ba18b3195e51
callum-marshall <callum_marshall@hotmail.co.uk>
2019-07-11 16:46:21 +0100

    add timing framework to benchmark and add myReverse

---
+++ src/benchmark.js
@@ -0,0 +1,31 @@
+class Benchmark {
+
+  time() {
+    return new Date(Date.now())
+  }
+
+  run(myFunction, array) {
+    const start = this.time()
+    myFunction(array)
+    const end = this.time()
+    const diff = end - start
+    return diff
+  }
+
+  generateArray(size) {
+    const array = Array(size).fill().map(() => Math.round(Math.random() * 1000))
+    return array
+  }
+
+  runManyTimes(myFunction) {
+    for (let size = 50000; size <= 1000000; size = size + 50000) {
+      const array = this.generateArray(size)
+      const diff = this.run(myFunction, array)
+      console.log(`|${size} || ${diff}`)
+    }
+  }
+}
+
+module.exports = {
+  Benchmark
+}
