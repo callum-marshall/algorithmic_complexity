b66fd56ea76ed9629554ce158dffb9d2b67d602c
callum-marshall <callum_marshall@hotmail.co.uk>
2019-07-19 17:19:36 +0100

    add fizzBuzz

---
+++ src/algorithms/fizzBuzz.js
@@ -0,0 +1,25 @@
+const { Benchmark } = require('../benchmark')
+
+const benchmark = new Benchmark
+
+const fizzBuzz = (array) => {
+
+  let answer = []
+
+  array.forEach(function (number) {
+    let string = ''
+    if (number % 3 === 0) string += 'Fizz'
+    if (number % 5 === 0) string += 'Buzz'
+    if (string.length === 0) string += `${number}`
+    answer.push(string)
+  })
+
+  return answer
+
+}
+
+const test = Array.from(Array(100), (_,x) => x + 1)
+
+console.log(fizzBuzz(test))
+
+benchmark.runManyTimes(fizzBuzz)
